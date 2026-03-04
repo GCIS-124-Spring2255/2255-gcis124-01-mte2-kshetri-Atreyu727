7a8aff80244295799c051a54c6e71c76cbe7aec3
Atreyu Convard (RIT Student) <alc7268@rit.edu>
2026-03-04 10:30:12 -0500

    Question 1

--- src/main/java/mte2/shapes/Circle.java
+++ src/main/java/mte2/shapes/Circle.java
@@ -3,18 +3,26 @@
 // File name: Circle.java
 
 package mte2.shapes;
-
-public class Circle {
-// public class Circle implements Shape {
+public class Circle implements Shape {
     
-    // private final double radius;
-    // public Circle(double radius) {    this.radius = radius;    }
+    private final double radius;
+    public Circle(double radius) {   this.radius = radius;    }
 
-    // @Override
-    // @Override
+    @Override
+    public double perimeter() {
+        return 2*Math.PI*radius;
+
+    }
+
+    @Override
+    public double area() {
+        return Math.PI*(Math.pow(radius, 2));
+    }
 
     public static void main(String[] args) {
-        
+        Circle c1 = new Circle(5);
+        System.out.println(c1.area());
+        System.out.println(c1.perimeter());
         // ...
 
     }
--- src/main/java/mte2/shapes/Triangle.java
+++ src/main/java/mte2/shapes/Triangle.java
@@ -4,21 +4,30 @@
 
 package mte2.shapes;
 
-public class Triangle {
-// public class Triangle implements Shape {
-    // private double sideA;
-    // private double sideB;
-    // private double sideC;
+public class Triangle implements Shape {
 
-    // public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
+    private double sideA;
+    private double sideB;
+    private double sideC;
 
-    // @Override
+    public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
 
     // @Override
-    // public double area() {    double p = perimeter()  / 2;    }
+    public double perimeter() {
+        return sideA+sideB+sideC;
+    }
+
+    // @Override
+    public double area() {
+        double p = perimeter()  / 2; 
+        return Math.sqrt(p*(p-sideA)*(p-sideB)*(p-sideC));
+    
+    }
 
     public static void main(String[] args) {
-        
+        Triangle t1 = new Triangle(3, 4, 5);
+        System.out.println(t1.perimeter());
+        System.out.println(t1.area());
         // ...
 
     }
